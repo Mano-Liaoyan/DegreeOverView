9112db6d0d0642ab7e571b445c87e1beba75eca4
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-05-06 16:47:19 +0800

    Move the backend files into api module

--- backend/DegreeOverView/urls.py
+++ backend/DegreeOverView/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import login, search
+from api import login, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
