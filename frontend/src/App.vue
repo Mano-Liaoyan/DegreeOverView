992638f9ef852ac39b6b4f0180b8cb5a9b52cdee
Orianna1014 <704216435@qq.com>
2021-05-09 00:11:35 +0800

    create frontend and connect ant-design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div> -->
+  <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">
+      Dashed
+    </a-button>
+    <a-button type="danger">
+      Danger
+    </a-button>
+    <a-config-provider :auto-insert-space-in-button="false">
+      <a-button type="primary">
+        按钮
+      </a-button>
+    </a-config-provider>
+    <a-button type="primary">
+      按钮
+    </a-button>
+    <a-button type="link">
+      Link
+    </a-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import { Button, message } from 'ant-design-vue'
+import App from './App'
+import router from './router'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Button)
+
+Vue.prototype.$message = message
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+// import Vue from 'vue'
+// import { Button, message } from 'ant-design-vue'
+// import App from './App'
+
+// Vue.config.productionTip = false
+
+// /* v1.1.2 */
+// Vue.component(Button.name, Button)
+// Vue.component(Button.Group.name, Button.Group)
+
+// /* v1.1.3+ 自动注册Button下组件，如Button.Group */
+// Vue.use(Button)
+
+// Vue.prototype.$message = message
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
