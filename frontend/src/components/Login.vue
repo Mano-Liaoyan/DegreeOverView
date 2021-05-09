c2032496cde5390f1bef53f2de98ec75b8b364ec
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-05-09 22:34:18 +0800

    front end login finish

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,7 +2,7 @@
   <div class="herVerCenter">
     <a-form-model layout="vertical" :model="formInline" @submit="handleSubmit" @submit.native.prevent>
       <a-form-model-item>
-        <a-input v-model="formInline.user" placeholder="Username">
+        <a-input v-model="formInline.username" placeholder="Username">
           <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)"/>
         </a-input>
       </a-form-model-item>
@@ -12,12 +12,11 @@
         </a-input>
       </a-form-model-item>
       <a-form-model-item>
-        <a-button type="primary" html-type="submit" :disabled="formInline.user === '' || formInline.password === ''" @click="goToHomePage">
+        <a-button type="primary" html-type="submit" :disabled="formInline.user === '' || formInline.password === ''">
           Log in
         </a-button>
       </a-form-model-item>
     </a-form-model>
-    {{ return_info }}
   </div>
 </template>
 
@@ -29,10 +28,9 @@ export default {
   data() {
     return {
       formInline: {
-        user: '',
+        username: '',
         password: '',
       },
-      return_info: null
     };
   },
   methods: {
@@ -40,9 +38,11 @@ export default {
       console.log(this.formInline);
       console.log(e);
       //异步访问api以获取数据
-      axios.get('http://127.0.0.1:8000/api/student/2/').then((res) => {
+      axios.post('http://127.0.0.1:8000/api/student/login/', this.formInline).then((res) => {
         console.log(res.data);
-        this.return_info = res.data;
+        if (res.data.code === 0) {
+          this.goToHomePage();
+        }
       }).catch((e) => {
         console.log(e);
       });
