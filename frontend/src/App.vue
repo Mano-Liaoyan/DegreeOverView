61a3a123c4591f3de1885deff6b9ae2c48cb8ac4
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-05-09 19:50:46 +0800

    restore the login view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>DegreeOverView</h1>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "@/components/Login";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
@@ -23,6 +24,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
+}
+
+.herVerCenter {
+  display: flex;
+  justify-content: center;
+  align-content: center;
 }
 </style>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-
+  <div class="herVerCenter">
+    <a-form-model layout="vertical" :model="formInline" @submit="handleSubmit" @submit.native.prevent>
+      <a-form-model-item>
+        <a-input v-model="formInline.user" placeholder="Username">
+          <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)"/>
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-input v-model="formInline.password" type="password" placeholder="Password">
+          <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)"/>
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button type="primary" html-type="submit" :disabled="formInline.user === '' || formInline.password === ''">
+          Log in
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      formInline: {
+        user: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      console.log(this.formInline);
+      console.log(e)
+    },
+  },
 }
 </script>
 
