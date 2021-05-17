23deb45799687ef2f905b9f776d68d985d7573d4
ldy127 <1773779147@qq.com>
2021-05-17 22:01:58 +0800

    designer

--- frontend/src/components/Designer.vue
+++ frontend/src/components/Designer.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="designer">
     <a-layout id="components-layout-demo-custom-trigger">
-      
-        <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
-          <div class="logo"/>
+      <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+        <div v-if="collapsed" id="logo-collapsed"></div>
+        <div v-else id="logo">DegreeOverView</div>
         <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
           <a-menu-item key="1">
             <a-icon type="user"/>
@@ -20,18 +20,23 @@
         </a-menu>
       </a-layout-sider>
       <a-layout>
-        <a-layout-header style="background: #fff; padding: 0"> 
-          <h2 style="margin-left: 30px; font-weight :bold;"> DegreeOverview </h2>
-          <a-icon
-            class="trigger"
-            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-            @click="() => (collapsed = !collapsed)"
-          />
-         
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-row type="flex" justify="space-between" align="middle">
+            <a-col>
+              <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="() => (this.collapsed = !collapsed)"/>
+            </a-col>
+            <!--Left top bar-->
+            <a-col>
+              <a-row>
+                <a-col>
+                  Welcome xxx!
+                  <a-icon class="top-left-buttons" type="logout" style="color: #BB4444"/>
+                </a-col>
+              </a-row>
+            </a-col>
+          </a-row>
         </a-layout-header>
-        <a-layout-content
-          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
-        >
+        <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
           Content
         </a-layout-content>
       </a-layout>
@@ -39,6 +44,7 @@
   </div>
 </template>
 <script>
+
 export default {
   data() {
     return {
@@ -55,17 +61,45 @@ export default {
   cursor: pointer;
   transition: color 0.3s;
 }
+
+.top-left-buttons {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px 0 6px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
 #components-layout-demo-custom-trigger .trigger:hover {
   color: #1890ff;
 }
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
+
+#components-layout-demo-custom-trigger #logo-collapsed {
+  height: 48px;
+  width: 48px;
+  margin: 16px auto;
+  background-image: url('../../static/images/timg.jpg');
+  -moz-background-size: 100% 100%;
+  background-size: 100% 100%;
 }
+
+#components-layout-demo-custom-trigger #logo {
+  height: 48px;
+  margin: 16px;
+  font-size: 20px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /*background-color: rgba(185, 104, 66, 0.26);*/
+  -moz-background-size: 100%;
+  background-size: 100%;
+}
+
 #components-layout-demo-custom-trigger {
   height: 100%;
 }
+
 #designer {
   height: 100%;
 }
