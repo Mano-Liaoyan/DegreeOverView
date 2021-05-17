d463d0e381379bfe8c06f50cf3b92a3287511f65
ldy127 <1773779147@qq.com>
2021-05-17 17:13:01 +0800

    designer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,8 +14,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;  
-  display: flex;
   color: #2c3e50;
   margin: auto;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- frontend/src/components/Designer.vue
+++ frontend/src/components/Designer.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="designer">
       <div class="top">
         <a-row type="flex" justify="space-between">
@@ -32,7 +32,7 @@
                   </a-select>
                 </a-form-item>
                 <a-form-item>
-                <!-- <a-button type="primary" html-type="submit" :disabled="hasErrors(form.getFieldsError())"> -->
+                <!-- <a-button type="primary" html-type="submit" :disabled="hasErrors(form.getFieldsError())"> 
                 <a-button type="primary" html-type="submit">
                   Search
                 </a-button>
@@ -60,9 +60,10 @@ export default {
     }
   }
 }
-</script>
+</script>-->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -96,4 +97,116 @@ a {
   width: 1000px;
 }
 
+</style>-->
+
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :default-selected-keys="['2']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          nav 1
+        </a-menu-item>
+        <a-menu-item key="2">
+          nav 2
+        </a-menu-item>
+        <a-menu-item key="3">
+          nav 3
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          mode="inline"
+          :default-selected-keys="['1']"
+          :default-open-keys="['sub1']"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-sub-menu key="sub1">
+            <span slot="title"><a-icon type="user" />subnav 1</span>
+            <a-menu-item key="1">
+              option1
+            </a-menu-item>
+            <a-menu-item key="2">
+              option2
+            </a-menu-item>
+            <a-menu-item key="3">
+              option3
+            </a-menu-item>
+            <a-menu-item key="4">
+              option4
+            </a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <span slot="title"><a-icon type="laptop" />subnav 2</span>
+            <a-menu-item key="5">
+              option5
+            </a-menu-item>
+            <a-menu-item key="6">
+              option6
+            </a-menu-item>
+            <a-menu-item key="7">
+              option7
+            </a-menu-item>
+            <a-menu-item key="8">
+              option8
+            </a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <span slot="title"><a-icon type="notification" />subnav 3</span>
+            <a-menu-item key="9">
+              option9
+            </a-menu-item>
+            <a-menu-item key="10">
+              option10
+            </a-menu-item>
+            <a-menu-item key="11">
+              option11
+            </a-menu-item>
+            <a-menu-item key="12">
+              option12
+            </a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
+
+<style>
+
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
 </style>
