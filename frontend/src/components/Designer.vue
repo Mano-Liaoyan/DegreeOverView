3351fdcfb3284a7ff07c7ed1e06580bbd8a69d3b
ldy127 <1773779147@qq.com>
2021-05-17 17:46:23 +0800

    designer

--- frontend/src/components/Designer.vue
+++ frontend/src/components/Designer.vue
@@ -128,50 +128,15 @@ a {
           :default-open-keys="['sub1']"
           :style="{ height: '100%', borderRight: 0 }"
         >
-          <a-sub-menu key="sub1">
-            <span slot="title"><a-icon type="user" />subnav 1</span>
-            <a-menu-item key="1">
-              option1
-            </a-menu-item>
-            <a-menu-item key="2">
-              option2
-            </a-menu-item>
-            <a-menu-item key="3">
-              option3
-            </a-menu-item>
-            <a-menu-item key="4">
-              option4
-            </a-menu-item>
-          </a-sub-menu>
+          <a-menu-item key="sub1">
+            <a-icon type="user" />
+             <span>Option 1</span>
+          </a-menu-item>
           <a-sub-menu key="sub2">
             <span slot="title"><a-icon type="laptop" />subnav 2</span>
-            <a-menu-item key="5">
-              option5
-            </a-menu-item>
-            <a-menu-item key="6">
-              option6
-            </a-menu-item>
-            <a-menu-item key="7">
-              option7
-            </a-menu-item>
-            <a-menu-item key="8">
-              option8
-            </a-menu-item>
           </a-sub-menu>
           <a-sub-menu key="sub3">
             <span slot="title"><a-icon type="notification" />subnav 3</span>
-            <a-menu-item key="9">
-              option9
-            </a-menu-item>
-            <a-menu-item key="10">
-              option10
-            </a-menu-item>
-            <a-menu-item key="11">
-              option11
-            </a-menu-item>
-            <a-menu-item key="12">
-              option12
-            </a-menu-item>
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
@@ -182,7 +147,7 @@ a {
           <a-breadcrumb-item>App</a-breadcrumb-item>
         </a-breadcrumb>
         <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+          :style="{ background: '#fff', padding: '24px', margin: 0 }"
         >
           Content
         </a-layout-content>
