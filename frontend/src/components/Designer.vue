a2d4874bf20d16d42f766b7d284df726d2c6d280
ldy127 <1773779147@qq.com>
2021-05-17 23:36:05 +0800

    designer

--- frontend/src/components/Designer.vue
+++ frontend/src/components/Designer.vue
@@ -3,33 +3,44 @@
     <a-layout id="components-layout-demo-custom-trigger">
       <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
         <div v-if="collapsed" id="logo-collapsed"></div>
-        <div v-else id="logo">DegreeOverView</div>
+        <div v-else id="logo"></div>
         <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
           <a-menu-item key="1">
-            <a-icon type="user"/>
-            <span>nav 1</span>
+            <a-icon type="plus"/>
+            <span>Create a new course</span>
           </a-menu-item>
           <a-menu-item key="2">
-            <a-icon type="video-camera"/>
-            <span>nav 2</span>
+            <a-icon type="edit"/>
+            <span>Modify a course</span>
           </a-menu-item>
           <a-menu-item key="3">
-            <a-icon type="upload"/>
-            <span>nav 3</span>
+            <a-icon type="tool"/>
+            <span>Define dependencies</span>
+          </a-menu-item>
+          <a-menu-item key="4">
+            <a-icon type="eye"/>
+            <span>View dependencies</span>
+          </a-menu-item>
+          <a-menu-item key="5">
+            <a-icon type="link"/>
+            <span>Define relationships</span>
           </a-menu-item>
         </a-menu>
       </a-layout-sider>
       <a-layout>
         <a-layout-header style="background: #fff; padding: 0">
           <a-row type="flex" justify="space-between" align="middle">
+            <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="() => (this.collapsed = !collapsed)"/>
             <a-col>
-              <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="() => (this.collapsed = !collapsed)"/>
+              <h2 style="font-weight :bold; margin: 0;"> DegreeOverView </h2>
             </a-col>
             <!--Left top bar-->
             <a-col>
               <a-row>
                 <a-col>
-                  Welcome xxx!
+                  <a-icon type="smile"></a-icon>
+                  Welcome xxx!   
+                  <a style="margin-left: 20px;">     Log out   </a>
                   <a-icon class="top-left-buttons" type="logout" style="color: #BB4444"/>
                 </a-col>
               </a-row>
