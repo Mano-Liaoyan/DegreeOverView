8d31c888e134c3827e4fc3bb0090f55c6a4da949
Orianna1014 <704216435@qq.com>
2021-05-19 22:29:19 +0800

    modify lecturer and create analysis page

---
+++ frontend/src/components/CourseListForDependency.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="clfd">
+    <template>
+      <a-table :columns="columns" :data-source="data">
+        <a slot="name" slot-scope="text">{{ text }}</a>
+        <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+        <span slot="tags" slot-scope="tags">
+          <a-tag
+            v-for="tag in tags"
+            :key="tag"
+            :color="
+              tag === 'loser'
+                ? 'volcano'
+                : tag.length > 5
+                ? 'geekblue'
+                : 'green'
+            "
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+        <span slot="action" slot-scope="text, record">
+          <a>Invite 一 {{ record.name }}</a>
+          <a-divider type="vertical" />
+          <a>Select</a>
+          <a-divider type="vertical" />
+          <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+        </span>
+      </a-table>
+    </template>
+  </div>
+</template>
+
+<script>
+const columns = [
+  {
+    title: "Course Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Major",
+    key: "tags",
+    dataIndex: "tags",
+    scopedSlots: { customRender: "tags" },
+  },
+  {
+    title: "Action",
+    key: "action",
+    scopedSlots: { customRender: "action" },
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    name: "Discrete Math",
+    tags: ["CST"],
+  },
+];
+
+export default {
+  name: "CLFD",
+  data() {
+    return {
+      data,
+      columns
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
