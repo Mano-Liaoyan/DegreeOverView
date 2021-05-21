1d4e67eddfaa1cf27ce47942c2d2b6467c03ecd9
ldy127 <1773779147@qq.com>
2021-05-21 21:59:15 +0800

    ...

--- frontend/src/components/Designer.vue
+++ frontend/src/components/Designer.vue
@@ -67,34 +67,6 @@
         </a-layout-content>
       </a-layout>
     </a-layout>
-
-    <!--
-      	Dialog 对话框
-     -->
-    <el-dialog :title="dialogTitle" :visible.sync="dialogFormVisible" width="35%">
-		  <el-form ref="tempData" :rules="rules" :model="tempForm" label-position="left" label-width="180px" style="width: 420px; margin-left:50px;">
-		    <el-form-item label="项目名称" prop="projectName">
-		      <el-input v-model="tempForm.project" placeholder="请输入项目名称~"/>
-		    </el-form-item>
-		    <el-form-item label="负责人" prop="projectManager">
-		      <el-input v-model="tempForm.manager" placeholder="请输入负责人~"/>
-		    </el-form-item>
-		    <el-form-item label="项目人员" prop="projectMember">
-		      <el-input v-model="tempForm.member" placeholder="请输入项目人员~"/>
-		    </el-form-item>
-		    <el-form-item label="项目描述" prop="projectDesc">
-		      <el-input v-model="tempForm.desc" placeholder="请输入项目描述~"/>
-		    </el-form-item>
-		  </el-form>
-		  <div slot="footer" class="dialog-footer">
-		    <el-button @click="dialogFormVisible = false">取 消</el-button>
-		    <el-button type="primary" @click="createData()">确 定</el-button>
-		  </div>
-    </el-dialog>
-    
-  	<!--
-      	结束
-     -->
   </div>
 </template>
 <script>
