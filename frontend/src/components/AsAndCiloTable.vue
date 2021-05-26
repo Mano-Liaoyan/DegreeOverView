4995bb187b08bf451cebab213763ca202b6c2e2e
Mano-Liaoyan <ben.jaminlyyf21@gmail.com>
2021-05-26 16:34:25 +0800

    add cilo table and modify the course page

---
+++ frontend/src/components/AsAndCiloTable.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <a-table :columns="columns" :data-source="data" :pagination="false" bordered rowKey="evam">
+      <!--    <a slot="name" slot-scope="text">{{ text }}</a>-->
+      <!--    <span slot="customTitle"><a-icon type="smile-o"/> Name</span>-->
+      <span slot="tags" slot-scope="tags">
+        <a-tag :closable="true" v-for="tag in tags" :key="tag" :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
+          {{ tag.toUpperCase() }}
+        </a-tag>
+      </span>
+      <span slot="action" slot-scope="text, record">
+        <a>Modify</a>
+        <a-divider type="vertical"/>
+        <a @click="clickDelete(record.evam)">Delete</a>
+        <a-divider type="vertical"/>
+        <a>Add CILO</a>
+      </span>
+    </a-table>
+    <div style="text-align: end;margin-top: 20px;">
+      <a-button class="editable-add-btn" @click="clickAdd()">
+        Add
+      </a-button>
+    </div>
+  </div>
+</template>
+
+<script>
+const columns = [
+  /*  {
+      dataIndex: 'name',
+      key: 'name',
+      slots: {title: 'customTitle'},
+      scopedSlots: {customRender: 'name'},
+      align: 'center'
+    },*/
+  {
+    title: 'Evaluation Method',
+    key: 'evam',
+    dataIndex: 'evam',
+    align: 'center'
+  },
+  {
+    title: 'Percentage',
+    dataIndex: 'percentage',
+    key: 'percentage',
+    align: 'center'
+  },
+  {
+    title: 'CILOs',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: {customRender: 'tags'},
+    align: 'center'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: {customRender: 'action'},
+    align: 'center'
+  },
+];
+
+let data = [
+  {
+    evam: 'Assignments/Quizzes',
+    percentage: '15%',
+    tags: ['nice', 'developer'],
+  },
+  {
+    evam: 'Labs',
+    percentage: '25%',
+    tags: ['loser'],
+  },
+  {
+    evam: 'Projects',
+    percentage: '10%',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    evam: 'Examination',
+    percentage: '50%',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+export default {
+  name: "AsAndCiloTable",
+  data() {
+    return {
+      data,
+      columns,
+      counter: 1
+    };
+  },
+  methods: {
+    clickAdd() {
+      this.data.push({
+        percentage: '',
+        tags: [],
+        evam: 'New Method' + this.counter
+      })
+      this.counter++;
+      console.log(this.data);
+    },
+    clickDelete(key) {
+      const dataSource = [...this.data];
+      this.data = dataSource.filter(item => item.evam !== key);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/components/ModifyCourse.vue
+++ frontend/src/components/ModifyCourse.vue
@@ -1,76 +1,112 @@
 <template>
-  <a-form-model ref="ruleForm" :model="form" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol">
-    <a-row>
-      <a-form-model-item ref="name" label="Course Name" prop="cname" validate-status="success">
-        <a-input v-model="form.cname" @blur="() => {$refs.name.onFieldBlur();}"/>
-      </a-form-model-item>
-      <a-form-model-item ref="name" label="Course Code" prop="code" validate-status="success">
-        <a-input v-model="form.code" @blur="() => {$refs.name.onFieldBlur();}"/>
-      </a-form-model-item>
-      <a-form-model-item label="Academic Start Year" prop="startDate">
-        <YearSelector v-model="form.startDate"></YearSelector>
-      </a-form-model-item>
+  <a-form-model ref="ruleForm" :model="form" :rules="rules" layout="vertical">
+    <!--    :label-col="labelCol" :wrapper-col="wrapperCol"-->
+    <a-row type="flex" justify="space-between">
+      <a-col class="horizTwo" :span="10">
+        <a-form-model-item ref="name" label="Course Name" prop="cname" validate-status="success">
+          <a-input v-model="form.cname" @blur="() => {$refs.name.onFieldBlur();}"/>
+        </a-form-model-item>
+      </a-col>
+      <a-col class="horizTwo" :span="10">
+        <a-form-model-item ref="name" label="Course Code" prop="code" validate-status="success">
+          <a-input v-model="form.code" @blur="() => {$refs.name.onFieldBlur();}"/>
+        </a-form-model-item>
+      </a-col>
     </a-row>
     <!--    <a-form-model-item ref="name" label="CILOs" prop="cilo">-->
     <!--      <a-input v-model="form.cilo" @blur="() => {$refs.name.onFieldBlur();}"/>-->
     <!--    </a-form-model-item>-->
     <!--    <DynamicForm></DynamicForm>-->
-    <a-form-model-item ref="name" label="Programme" prop="programme" style="margin-top: -10px">
-      <a-input v-model="form.programme" @blur="() => {$refs.name.onFieldBlur();}"/>
-    </a-form-model-item>
-    <a-form-model-item ref="name" label="Prerequisites" prop="prerequisites">
-      <a-input v-model="form.prerequisites" @blur="() => {$refs.name.onFieldBlur();}"/>
-    </a-form-model-item>
-    <a-form-model-item ref="name" label="Assessments&CILOs" prop="cilo">
-      <!--      <aaa></aaa>-->
-    </a-form-model-item>
-    <a-form-model-item label="Type" prop="type">
-      <a-radio-group v-model="form.type">
-        <a-radio value="1">
-          Major Required (MR)
-        </a-radio>
-        <a-radio value="2">
-          Major Election (ME)
-        </a-radio>
-        <br>
-        <a-radio value="3">
-          Gradual Election (GE)
-        </a-radio>
-        <a-radio value="4">
-          Free Election (FE)
-        </a-radio>
-      </a-radio-group>
-    </a-form-model-item>
-    <a-form-model-item :wrapper-col="{ span: 8, offset: 14 }">
-      <a-button type="primary" @click="onSubmit">
-        Modify
-      </a-button>
-      <a-button style="margin-left: 10px;" @click="resetForm">
-        Reset
-      </a-button>
-    </a-form-model-item>
+    <a-row type="flex" justify="space-between">
+      <a-col class="horizTwo" :span="10">
+        <a-form-model-item label="Academic Start Year" prop="startDate">
+          <YearSelector v-model="form.startDate"></YearSelector>
+        </a-form-model-item>
+      </a-col>
+      <a-col class="horizTwo" :span="10">
+        <a-form-model-item label="Program" prop="program">
+          <a-select v-model="form.program" placeholder="please select your programme">
+            <a-select-option v-for="(item, index) in program_data" :key="index" :value="item">
+              {{ item }}
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+      </a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+      <a-col class="horizTwo" :span="22">
+        <a-form-model-item ref="name" label="Prerequisites" prop="prerequisites">
+          <a-input v-model="form.prerequisites" @blur="() => {$refs.name.onFieldBlur();}"/>
+        </a-form-model-item>
+      </a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+      <a-col class="horizTwo" :span="22">
+        <a-form-model-item ref="name" label="Assessments&CILOs" prop="cilo" style="margin-bottom: 0;">
+          <AsAndCiloTable/>
+        </a-form-model-item>
+      </a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+      <a-col class="horizTwo" :span="22">
+        <a-form-model-item label="Type" prop="type">
+          <a-radio-group v-model="form.type">
+            <a-radio value="1">
+              Major Required (MR)
+            </a-radio>
+            <a-radio value="2">
+              Major Election (ME)
+            </a-radio>
+            <a-radio value="3">
+              Gradual Election (GE)
+            </a-radio>
+            <a-radio value="4">
+              Free Election (FE)
+            </a-radio>
+          </a-radio-group>
+        </a-form-model-item>
+      </a-col>
+    </a-row>
+    <a-row type="flex" justify="end">
+      <a-col :span="12">
+        <a-form-model-item :wrapper-col="{ span: 8, offset: 14 }">
+          <a-button type="primary" @click="onSubmit">
+            Modify
+          </a-button>
+          <a-button style="margin-left: 10px;" @click="resetForm">
+            Reset
+          </a-button>
+        </a-form-model-item>
+      </a-col>
+    </a-row>
   </a-form-model>
 </template>
 <script>
 import YearSelector from "./YearSelector";
 import DynamicForm from "./DynamicForm";
 import aaa from "./aaa";
+import AsAndCiloTable from "./AsAndCiloTable";
+import axios from "axios";
 
 
 export default {
   name: "ModifyCourse",
   components: {
-    YearSelector, DynamicForm, aaa
+    YearSelector, DynamicForm, aaa, AsAndCiloTable
+  },
+  mounted() {
+    this.setProgramData();
   },
   data() {
     return {
-      labelCol: {span: 4},
-      wrapperCol: {span: 14},
+      labelCol: {span: 24},
+      wrapperCol: {span: 12},
+      program_data: undefined,
       form: {
         cname: '',
         code: '',
         cilo: '',
-        programme: '',
+        program: '',
         prerequisites: '',
         relation: '',
         startDate: undefined,
@@ -87,7 +123,7 @@ export default {
         cilo: [
           {required: true, message: 'Please input the cilo name!', trigger: 'blur'},
         ],
-        programme: [
+        program: [
           {required: true, message: 'Please input the programme name!', trigger: 'blur'},
         ],
         type: [
@@ -110,9 +146,31 @@ export default {
     resetForm() {
       this.$refs.ruleForm.resetFields();
     },
+    async setProgramData() {
+      let url = "http://127.0.0.1:8000/api/course/get_all_program/";
+      let data;
+      //异步访问cilo search api以获取数据
+      await axios.get(url).then((res) => {
+        // console.log(res.data);
+        if (res.data) { // If success then
+          // console.log(res.data);
+          this.program_data = res.data.programs
+        } else {// If not then
+          // console.log(res.data);
+          data = res.data;
+          this.program_data = undefined
+        }
+      }).catch((e) => {
+        // If some error occurs
+        console.log(e);
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+.horizTwo {
+  margin: auto
+}
 </style>
