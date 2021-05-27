464f4ac786068e5f503e54e36c6c6a66169e8d38
Orianna1014 <704216435@qq.com>
2021-05-27 22:07:29 +0800

    modify homepage

---
+++ frontend/src/components/Homepage.vue
@@ -0,0 +1,41 @@
+<template>
+  <a-carousel autoplay>
+    <div>
+        <img src="../../static/images/campus01.jpg">
+    </div>
+    <div>
+        <img src="../../static/images/campus02.jpg">
+    </div>
+    <div>
+        <img src="../../static/images/campus03.jpg">
+    </div>
+    <div>
+        <img src="../../static/images/campus04.jpg">
+    </div>
+  </a-carousel>
+</template>
+
+<script>
+export default {
+  name: "Homepage",
+  data() {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* For demo */
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 560px;
+  line-height: 560px;
+  overflow: hidden;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+}
+</style>
