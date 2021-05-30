75efb8479afc885757b0b1a1e5c2089cfc698592
ldy127 <1773779147@qq.com>
2021-05-30 21:34:48 +0800

    relation graph

---
+++ frontend/src/components/relationgraph.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div ref="myPage" style="height:calc(100vh - 50px);">
+      <SeeksRelationGraph
+        ref="seeksRelationGraph"
+        :options="graphOptions"
+        :on-node-click="onNodeClick"
+        :on-line-click="onLineClick">
+        <div slot="node" slot-scope="{node}" @mouseover="showNodeTips(node, $event)" @mouseout="hideNodeTips(node, $event)">
+          <div style="height:64px;line-height: 60px;border-radius: 32px">
+            <p style="font-size: 14px; text-align: center;">
+              {{ node.text }}
+            </p>
+          </div>
+          <div style="color: forestgreen;font-size: 16px;position: absolute;width: 160px;height:25px;line-height: 25px;margin-top:5px;margin-left:-48px;text-align: center;background-color: rgba(66,187,66,0.2);">
+            {{ node.data.course }}
+          </div>
+        </div>
+      </SeeksRelationGraph>
+    </div>
+    <div v-if="isShowNodeTipsPanel" :style="{left: nodeMenuPanelPosition.x + 'px', top: nodeMenuPanelPosition.y + 'px' }" style="z-index: 999;padding:10px;background-color: #ffffff;border:#eeeeee solid 1px;box-shadow: 0px 0px 8px #cccccc;position: absolute;">
+      <div style="line-height: 25px;padding-left: 10px;color: #888888;font-size: 12px;">{{currentNode.text}}</div>
+      <div class="c-node-menu-item">content:{{currentNode.data.content}}</div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import SeeksRelationGraph from 'relation-graph'
+export default {
+  name: 'Demo',
+  components: { SeeksRelationGraph },
+  data() {
+    return {
+      isShowCodePanel: false,
+      isShowNodeTipsPanel: false,
+      nodeMenuPanelPosition: { x: 0, y: 0 },
+      currentNode: {},
+      graphOptions: {
+        allowSwitchLineShape: true,
+        allowSwitchJunctionPoint: true,
+        defaultJunctionPoint: 'border'
+        // 这里可以参考"Graph 图谱"中的参数进行设置
+      }
+    }
+  },
+  mounted() {
+    this.showSeeksGraph()
+  },
+  methods: {
+    showSeeksGraph(query) {
+      var __graph_json_data = {
+        rootId: '2',
+        nodes: [
+          // 注意：在节点配置信息中，你的自定义属性需要像下面这样放到data标签中，否则数据会丢失
+          { id: '1', text: 'CILO-1', data: { course: 'discrete structure', content: 'dfcjsldvsdnv'} },
+          { id: '2', text: 'CILO-2', data: { course: 'java', content: 'dfcesfcesfesfv'} },
+          { id: '3', text: 'CILO-3', data: { course: 'oop', content: 'fcrvgdr'} },
+          { id: '4', text: 'CILO-4', data: { course: 'c programming', content: 'efcefcadcaccdc'} },
+          
+        ],
+        links: [
+          { from: '1', to: '2', text: 'dependence on' },
+          { from: '2', to: '3', text: 'dependence on' },
+          { from: '3', to: '4', text: 'dependence on' },
+          { from: '1', to: '3', text: 'dependence on' },
+          { from: '2', to: '4', text: 'dependence on' },
+          
+        ]
+      }
+      this.$refs.seeksRelationGraph.setJsonData(__graph_json_data, (seeksRGGraph) => {
+        // 这些写上当图谱初始化完成后需要执行的代码
+      })
+    },
+    onNodeClick(nodeObject, $event) {
+      console.log('onNodeClick:', nodeObject)
+    },
+    onLineClick(lineObject, $event) {
+      console.log('onLineClick:', lineObject)
+    },
+    showNodeTips(nodeObject, $event) {
+      this.currentNode = nodeObject
+      var _base_position = this.$refs.myPage.getBoundingClientRect()
+      console.log('showNodeMenus:', $event, _base_position)
+      this.isShowNodeTipsPanel = true
+      this.nodeMenuPanelPosition.x = $event.clientX - _base_position.x + 10
+      this.nodeMenuPanelPosition.y = $event.clientY - _base_position.y + 10
+    },
+    hideNodeTips(nodeObject, $event) {
+      this.isShowNodeTipsPanel = false
+    }
+  }
+}
+</script>
+
