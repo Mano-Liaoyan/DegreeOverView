c7dd3ea9ed8eccfa64780d96e62eb5dd46337be8
ldy127 <1773779147@qq.com>
2021-05-31 09:37:10 +0800

    a fixed performance result

--- frontend/src/components/performanceChart.vue
+++ frontend/src/components/performanceChart.vue
@@ -14,135 +14,39 @@
       let this_ = this;
       let myChart = echarts.init(document.getElementById('chart'));
       let option = {
-        color: [ '#67F9D8', '#FFE434', '#56A3F1', '#FF917C'],
+        color: [ '#FF917C'],
     title: {
         text: this.$route.params.coursename,
     },
     legend: {
-        data: ['Data A', 'Data B', 'Data C', 'Data D']
+        data: ['CILOs']
     },
-    radar: [
+    radar: 
         {
             indicator: [
-                { text: '指标一' },
-                { text: '指标二' },
-                { text: '指标三' },
-                { text: '指标四' },
-                { text: '指标五' }
+            { name: 'CILO 1', max: 100},
+            { name: 'CILO2', max: 100},
+            { name: 'CILO3', max: 100},
+           
             ],
-            center: ['25%', '50%'],
-            radius: 120,
-            startAngle: 90,
-            splitNumber: 4,
-            shape: 'circle',
-            name: {
-                formatter: '【{value}】',
-                textStyle: {
-                    color: '#428BD4'
-                }
-            },
-            splitArea: {
-                areaStyle: {
-                    color: ['#77EADF', '#26C3BE', '#64AFE9', '#428BD4'],
-                    shadowColor: 'rgba(0, 0, 0, 0.2)',
-                    shadowBlur: 10
-                }
-            },
-            axisLine: {
-                lineStyle: {
-                    color: 'rgba(211, 253, 250, 0.8)'
-                }
-            },
-            splitLine: {
-                lineStyle: {
-                    color: 'rgba(211, 253, 250, 0.8)'
-                }
-            }
         },
-        {
-            indicator: [
-                { text: '语文', max: 150 },
-                { text: '数学', max: 150 },
-                { text: '英语', max: 150 },
-                { text: '物理', max: 120 },
-                { text: '化学', max: 108 },
-                { text: '生物', max: 72 }
-            ],
-            center: ['75%', '50%'],
-            radius: 120,
-            name: {
-                textStyle: {
-                    color: '#fff',
-                    backgroundColor: '#666',
-                    borderRadius: 3,
-                    padding: [3, 5]
-                }
-            }
-
-        }
-    ],
-    series: [
-        {
-            name: '雷达图',
-            type: 'radar',
-            emphasis: {
-                lineStyle: {
-                    width: 4
-                }
-            },
-            data: [
-                {
-                    value: [100, 8, 0.40, -80, 2000],
-                    name: 'Data A'
-                },
-                {
-                    value: [60, 5, 0.30, -100, 1500],
-                    name: 'Data B',
-                    areaStyle: {
-                        color: 'rgba(255, 228, 52, 0.6)'
-                    }
-                }
-            ]
-        },
-        {
-            name: '成绩单',
-            type: 'radar',
-            radarIndex: 1,
-            data: [
-                {
-                    value: [120, 118, 130, 100, 99, 70],
-                    name: 'Data C',
-                    symbol: 'rect',
-                    symbolSize: 12,
-                    lineStyle: {
-                        type: 'dashed'
-                    },
-                    label: {
+    series: [{
+        name: '',
+        type: 'radar',
+        data: [
+            {
+                value: [98.5, 99.6, 97.0],
+                name: 'CILOs',
+                 label: {
                         show: true,
                         formatter: function (params) {
                             return params.value;
                         }
-                    }
-                },
-                {
-                    value: [100, 93, 50, 90, 70, 60],
-                    name: 'Data D',
-                    areaStyle: {
-                        color: new echarts.graphic.RadialGradient(0.1, 0.6, 1, [
-                            {
-                                color: 'rgba(255, 145, 124, 0.1)',
-                                offset: 0
-                            },
-                            {
-                                color: 'rgba(255, 145, 124, 0.9)',
-                                offset: 1
-                            }
-                        ])
-                    }
-                }
-            ]
-        }
-    ]
+                    },
+            },
+          
+        ]
+    }]
       };
       myChart.setOption(option);
  
